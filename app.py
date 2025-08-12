07bbfe988375fb77d43964b40a0de0faff561105
Marcelo Camicha <profemarcelocamicha@gmail.com>
2025-08-12 15:26:13 -0300

    before_first_request

--- app.py
+++ app.py
@@ -80,19 +80,12 @@ def listar_tablas():
     return jsonify(tablas)
 
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 
 if __name__ == '__main__':
-    # with app.app_context():
-    #     db.create_all()
-
-    with app.app_context():
-        try:
-            db.create_all()
-            print("Tablas creadas correctamente")
-        except Exception as e:
-            print("Error:", e)
-
     app.run(host='0.0.0.0', port=5000)
+
+
