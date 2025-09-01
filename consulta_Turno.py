06eebfa51662b8e8519bc4c53701e0d0f972b341
Marcelo Camicha <profemarcelocamicha@gmail.com>
2025-09-01 20:27:36 -0300

    merge funcion1 y 2

--- consulta_Turno.py
+++ consulta_Turno.py
@@ -2,11 +2,12 @@ import requests
 
 # url = 'http://localhost:5000/api/turnos/<id>'
 url = 'https://clinica-api-0n5q.onrender.com/api/turnos/2'
-def funcion1(){
+
+def funcion1():{
     print("funcion1")
 }
 
-def funcion2(){
+def funcion2():{
     print("funcion2")
 }
 
