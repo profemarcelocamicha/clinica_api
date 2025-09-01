da6da6e39dbb85d3da545a558634854321dccfe2
Marcelo Camicha <profemarcelocamicha@gmail.com>
2025-09-01 13:48:03 -0300

    Se hace un commit desde funcion2

--- consulta_Turno.py
+++ consulta_Turno.py
@@ -3,7 +3,8 @@ import requests
 # url = 'http://localhost:5000/api/turnos/<id>'
 url = 'https://clinica-api-0n5q.onrender.com/api/turnos/2'
 
-
+# respuesta
+# modificación funcion2
 
 response = requests.get(url)
 print(response.json())
