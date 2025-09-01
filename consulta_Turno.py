1741f97f090f3775d5cd41c9eb3f839ec0abd7ef
Marcelo Camicha <profemarcelocamicha@gmail.com>
2025-09-01 20:19:54 -0300

    funcion2 en consulta_Turno.py

--- consulta_Turno.py
+++ consulta_Turno.py
@@ -3,6 +3,11 @@ import requests
 # url = 'http://localhost:5000/api/turnos/<id>'
 url = 'https://clinica-api-0n5q.onrender.com/api/turnos/2'
 
+def funcion2(){
+    print("funcion2")
+}
+
+
 response = requests.get(url)
 print(response.json())
 
