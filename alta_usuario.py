10826c2fda55c99baa0f3850459209bfe5bdc656
Marcelo Camicha <profemarcelocamicha@gmail.com>
2025-10-13 17:02:04 -0300

    alta_usuario.py

---
+++ alta_usuario.py
@@ -0,0 +1,12 @@
+import requests, json
+
+url = "https://clinica-api-0n5q.onrender.com/api/usuarios"
+data = {
+    "userId": "prueba123",
+    "email": "test@gmail.com",
+    "token_fcm": "abcdefg"
+}
+resp = requests.post(url, json=data)
+
+print(resp.status_code)
+print(resp.text)
