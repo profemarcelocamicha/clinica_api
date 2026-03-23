21cb9217f640da0828d95aa62919f40dcd5e8382
Marcelo Camicha <profemarcelocamicha@gmail.com>
2026-03-23 15:29:04 -0300

    modificaciones varias

--- app/consulta_usuarios.py
+++ app/consulta_usuarios.py
@@ -1,7 +1,6 @@
 import requests
 
-
-# URL = "https://clinica-api-0n5q.onrender.com/api/usuarios"
+# url = "https://clinica-api-0n5q.onrender.com/api/usuarios"
 url = "https://clinica-api-dev.onrender.com/api/usuarios"
 
 response = requests.get(url)
