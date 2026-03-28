4fb6f40f24c4dde015f5da1fb50f5938a226c635
Marcelo Camicha <profemarcelocamicha@gmail.com>
2026-03-28 18:07:29 -0300

    cambio en prueba de integración. Se agrega variable de entorno, fecha y horaA

--- tests/Integration/test_turnos_api.py
+++ tests/Integration/test_turnos_api.py
@@ -1,15 +1,25 @@
 import pytest
+import os
+from datetime import datetime
 from app.alta_turno import crear_turno
 
 
 @pytest.mark.integration
 def test_crear_turno_real():
+    # Obtener entorno (Render o local)
+    env = os.getenv("ENV", "dev_local")
+
+    # Fecha y hora actuales
+    now = datetime.now()
+    fecha_actual = now.strftime("%Y-%m-%d")
+    hora_actual = now.strftime("%H:%M")
+
     data = {
-        "paciente": "Test Integracion",
-        "email": "test@test.com",
+        "paciente": f"Test {env.upper()}",
+        "email": f"test_{env}@test.com",
         "medico": "Doc",
-        "fecha": "2026-01-01",
-        "hora": "10:00"
+        "fecha": fecha_actual,
+        "hora": hora_actual
     }
 
     response = crear_turno(data)
