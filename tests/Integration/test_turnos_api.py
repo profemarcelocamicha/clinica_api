9a1db43b4ba346033f59f417541d04df240976f3
Marcelo Camicha <profemarcelocamicha@gmail.com>
2026-03-28 18:58:12 -0300

    cambio en prueba de integración. fecha y hora

--- tests/Integration/test_turnos_api.py
+++ tests/Integration/test_turnos_api.py
@@ -1,13 +1,10 @@
 import pytest
-import os
 from datetime import datetime
 from app.alta_turno import crear_turno
 
 
 @pytest.mark.integration
 def test_crear_turno_real():
-    # Obtener entorno (Render o local)
-    env = os.getenv("ENV", "dev_local")
 
     # Fecha y hora actuales
     now = datetime.now()
@@ -15,8 +12,8 @@ def test_crear_turno_real():
     hora_actual = now.strftime("%H:%M")
 
     data = {
-        "paciente": f"Test {env.upper()}",
-        "email": f"test_{env}@test.com",
+        "paciente": "Test",
+        "email": "test@test.com",
         "medico": "Doc",
         "fecha": fecha_actual,
         "hora": hora_actual
